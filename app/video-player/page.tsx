ae07739c06a265d845861bfb21a95a58d5bb1114
DeepakSxn <deepaksaxena192837@gmail.com>
2025-05-03 17:29:51 +0530

    a

--- app/video-player/page.tsx
+++ app/video-player/page.tsx
@@ -1290,7 +1290,7 @@ useEffect(() => {
       <header className="border-b sticky top-0 z-10 bg-background/95 backdrop-blur supports-[backdrop-filter]:bg-background/60">
         <div className="container flex h-16 items-center justify-between px-4 sm:px-6 lg:px-8">
         <Link href="/">
-           <img src="light.webp" alt="logo" />
+          <Logo/>
            </Link>
           <div className="flex items-center gap-4">
 
