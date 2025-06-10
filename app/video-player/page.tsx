9b72f2c908ae758bd330b693b747115c37ed3a64
Aksh Bahl <bahlaksh@gmail.com>
2025-06-10 16:30:38 +0530

    logo

--- app/video-player/page.tsx
+++ app/video-player/page.tsx
@@ -1432,8 +1432,8 @@ export default function VideoPlayerPage() {
       <header className="border-b sticky top-0 z-10 bg-background/95 backdrop-blur supports-[backdrop-filter]:bg-background/60">
         <div className="container flex h-16 items-center justify-between px-4 sm:px-6 lg:px-8">
         <Link href="/">
-          <Logo/>
-           </Link>
+          <img src="light.webp" height={40} width={120} alt="EOXS Logo" style={{objectFit: 'contain'}} />
+        </Link>
           <div className="flex items-center gap-4">
 
             <TooltipProvider>
